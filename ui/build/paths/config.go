f8ba53806c87013f097db8feb61faac5d2f95e97
Pranav Vashi <neobuddy89@gmail.com>
2020-04-26 13:10:22 +0530

    soong: Whitelist perl usage
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I65a204c6936b6c9cf3d26df505b92deb20a960ae

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -101,6 +101,7 @@ var Configuration = map[string]PathConfig{
 	"nproc":    Allowed,
 	"openssl":  Allowed,
 	"patch":    Allowed,
+	"perl":     Allowed,
 	"pkg-config": Allowed,
 	"pstree":   Allowed,
 	"python3":  Allowed,

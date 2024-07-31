fab10513a34c50708f1d6c9f6de363a7b38ff1a4
Inseob Kim <inseob@google.com>
2024-07-31 02:01:03 +0000

    Revert^2 "Conditionally pass kernel version to build.prop"
    This reverts commit 29fed1efe85cc07bb4311513e4acc1da73dfb298.
    
    Reason for revert: Fixed breakage with aosp/3200730, verified with ABTD
    
    Change-Id: I207abd188636673250eb9a070be95db0b82f39e7

--- android/variable.go
+++ android/variable.go
@@ -522,6 +522,8 @@ type ProductVariables struct {
 	ArtTargetIncludeDebugBuild *bool `json:",omitempty"`
 
 	SystemPropFiles []string `json:",omitempty"`
+
+	EnableUffdGc *string `json:",omitempty"`
 }
 
 type PartitionQualifiedVariablesType struct {
